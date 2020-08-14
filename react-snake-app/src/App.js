bdef25580e52bc46dfba30f55b54d5706083ba12
Chang <changwh97@gmail.com>
2020-08-14 15:26:17 +0800

    Update React Snake Game
    learn from tutorial "https://www.youtube.com/watch?v=-oOgsGP3t5o"
    author: Chigabiga

--- react-snake-app/src/App.js
+++ react-snake-app/src/App.js
@@ -11,21 +11,29 @@ const getRandomCoordinates = () => {
   return [x,y]
 }
 
+const initialState = {
+  food: getRandomCoordinates(),
+  speed: 200,
+  direction: 'RIGHT',
+  snakeDots: [
+    [0,0],
+    [2,0]
+  ]
+}
+
 class App extends Component {
 
-  state = {
-    food: getRandomCoordinates(),
-    speed: 200,
-    direction: 'RIGHT',
-    snakeDots: [
-      [0,0],
-      [2,0]
-    ]
-  }
+  state = initialState;
 
 componentDidMount() {
   setInterval(this.moveSnake, this.state.speed);
   document.onkeydown = this.onKeyDown;
+}
+
+componentDidUpdate(){
+  this.checkIfOutOfBorders();
+  this.checkIfCollapsed();
+  this.checkIfEat();
 }
 
   onKeyDown = (e) => {
@@ -85,10 +93,53 @@ componentDidMount() {
     }
   }
 
+  checkIfCollapsed(){
+    let snake = [...this.state.snakeDots];
+    let head = snake[snake.length-1];
+
+    snake.pop();
+    snake.forEach(dot => {
+      if(head[0] === dot[0] && head[1] === dot[1] ){
+        this.onGameOver();
+      }
+    })
+  }
+
+  checkIfEat () {
+    let head = this.state.snakeDots[this.state.snakeDots.length -1];
+    let food = this.state.food;
+
+    if(head[0] === food[0] && head[1] === food[1] ){
+      this.setState({
+        food: getRandomCoordinates()
+      })
+        this.extendSnake();
+        this.accelerator();
+      }
+
+  }
+
+  extendSnake() {
+    let newSnakeBody = [...this.state.snakeDots];
+    newSnakeBody.unshift([])
+    this.setState({
+      snakeDots: newSnakeBody
+    })
+  }
+
+  accelerator() {
+    if (this.state.speed > 10) {
+      this.setState({
+        speed: this.state.speed -10
+      })
+    }
+  }
+
   onGameOver() {
     alert (`Game Over. Snake length is ${this.state.snakeDots.length}`);
-    
+    this.setState(initialState)
   }
+
   render(){
   return (
     <div className="game-area">
